391b325b155caae300cd3d8ad25386e14b8b3447
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2023-10-03 19:41:10 +0330

    Solution v2 (using `eors` instruction)

--- Src/main.c
+++ Src/main.c
@@ -53,13 +53,39 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+inline void pc14_init(void)
+{
+  // Load required values in registers using assembly instructions
+  asm volatile(
+      "ldr r0, =0x4001100C\n" // Load GPIOC_ODR address into register r0
+      "ldr r1, [r0]\n"        // Load value in the GPIOC_ODR address into register r1
+      "mov r2, #0x00004000\n" // Move the value of bit mask for PC14 into register r2
+  );
+}
 
+inline void toggle_v1(void)
+{
+  // Toggle PC14 using assembly instructions
+  asm volatile(
+      "eor r1, r2\n"   // XOR the last value of GPIOC_ODR (r1) with the mask (r2)
+      "str r1, [r0]\n" // Store the new value (r1) back to the address of GPIOC_ODR
+  );
+}
+
+inline void toggle_v2(void)
+{
+  // Toggle PC14 using assembly instructions
+  asm volatile(
+      "eors r1, r2\n"  // XOR the last value of GPIOC_ODR (r1) with the mask (r2)
+      "str r1, [r0]\n" // Store the new value (r1) back to the address of GPIOC_ODR
+  );
+}
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -91,54 +117,52 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
 
-    // Load required values in registers using assembly instructions
-    asm volatile(
-        "ldr r0, =0x4001100C\n" // GPIOC_ODR address
-        "ldr r1, [r0]\n"        // Load GPIOC_ODR value
-        "ldr r2, =0x00004000\n" // Bit 14 mask
-    );
+  pc14_init();
 
-    while (1)
-    {
-        // Toggle PC14 using assembly instructions
-        asm volatile(
-            "eor r1, r1, r2\n" // XOR the value with the mask
-            "str r1, [r0]\n"   // Store the new value back to GPIOC_ODR
-        );
+  while (1)
+  {
+    toggle_v2();
 
-        // Frequency of the square wave generated on PC14: 
-        // With HSE = 8MHz :
-        // - RCC_PLL_MUL9 => f_HCLK = 72 MHz => f_PC14 = 5.15 MHz (stable)
-        // - RCC_PLL_MUL10 => f_HCLK = 80 MHz => f_PC14 = 5.75 MHz (stable)
-        // - RCC_PLL_MUL11 => f_HCLK = 88 MHz => f_PC14 = 6.25 MHz (stable)
-        // - RCC_PLL_MUL12 => f_HCLK = 96 MHz => f_PC14 = 6.85 MHz (stable)
-        // - RCC_PLL_MUL13 => f_HCLK = 104 MHz => f_PC14 = 7.4 MHz (stable)
-        // - RCC_PLL_MUL14 => f_HCLK = 112 MHz => f_PC14 = 8.0 MHz (stable)
-        // - RCC_PLL_MUL15 => f_HCLK = 120 MHz => f_PC14 = 8.5 MHz (stable)
-        // - RCC_PLL_MUL16 => f_HCLK = 128 MHz => f_PC14 = 9.1 MHz (stable)
-        // With HSE = 16MHz :
-        // - RCC_PLL_MUL9 => f_HCLK = 144 MHz => f_PC14 = 10.3 MHz (stable) => MAXIMUM STABLE FREQUENCY
-        // - RCC_PLL_MUL10 => f_HCLK = 160 MHz => f_PC14 = 11.5 MHz (unstable: stops about 20 seconds after startup)
+    // Frequency of the square wave generated on PC14 using toggle_v1():
+    // With HSE = 8MHz :
+    // - RCC_PLL_MUL9 => f_HCLK = 72 MHz => f_PC14 = 5.15 MHz (stable)
+    // - RCC_PLL_MUL10 => f_HCLK = 80 MHz => f_PC14 = 5.75 MHz (stable)
+    // - RCC_PLL_MUL11 => f_HCLK = 88 MHz => f_PC14 = 6.25 MHz (stable)
+    // - RCC_PLL_MUL12 => f_HCLK = 96 MHz => f_PC14 = 6.85 MHz (stable)
+    // - RCC_PLL_MUL13 => f_HCLK = 104 MHz => f_PC14 = 7.4 MHz (stable)
+    // - RCC_PLL_MUL14 => f_HCLK = 112 MHz => f_PC14 = 8.0 MHz (stable)
+    // - RCC_PLL_MUL15 => f_HCLK = 120 MHz => f_PC14 = 8.5 MHz (stable)
+    // - RCC_PLL_MUL16 => f_HCLK = 128 MHz => f_PC14 = 9.1 MHz (stable)
+    // With HSE = 16MHz :
+    // - RCC_PLL_MUL9 => f_HCLK = 144 MHz => f_PC14 = 10.3 MHz (stable) => MAXIMUM STABLE FREQUENCY
+    // - RCC_PLL_MUL10 => f_HCLK = 160 MHz => f_PC14 = 11.5 MHz (unstable: stops about 20 seconds after startup)
+
+    // Frequency of the square wave generated on PC14 using toggle_v2():
+    // With HSE = 8MHz :
+    // - RCC_PLL_MUL16 => f_HCLK = 128 MHz => f_PC14 = 10.6 MHz (stable)
+    // With HSE = 16MHz :
+    // - RCC_PLL_MUL9 => f_HCLK = 144 MHz => f_PC14 = 12.0 MHz (stable) => MAXIMUM STABLE FREQUENCY
+    // - RCC_PLL_MUL10 => f_HCLK = 160 MHz => f_PC14 = 13.3 MHz (unstable: stops about 20 seconds after startup)
 
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    }
+  }
   /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
@@ -152,9 +176,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -167,10 +190,10 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -189,7 +212,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
 }
 
 /* USER CODE BEGIN 4 */
@@ -197,33 +219,33 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-    /* User can add his own implementation to report the HAL error return state */
-    __disable_irq();
-    while (1)
-    {
-    }
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-    /* User can add his own implementation to report the file name and line number,
-       ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
